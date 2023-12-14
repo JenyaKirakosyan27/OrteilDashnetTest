261aa51646b06065eebbbb675364daa484a481ad
Jenya Kirakosyan <jenyakirakosyan27@gmail.com>
2023-12-14 14:36:46 +0400

    Test language select button and click on cookies

--- tests_/testLanguageSelectButtonandClickonCookie.py
+++ tests_/testLanguageSelectButtonandClickonCookie.py
@@ -8,6 +8,17 @@ class ClicOnCookieTest(BaseTest):
     def test_clik_on_cookie(self):
         navigationBarObj = NavigationBarPage(self.driver)
         navigationBarObj.click_the_Select_language_button()
-        sleep(10)
+        sleep(10) #time is needed for the cookie button to activate
         navigationBarObj.click_to_big_cookie_button()
         sleep(10)
+
+
+
+    def test_10000_clik_on_cookie(self):
+        navigationBarObj = NavigationBarPage(self.driver)
+        navigationBarObj.click_the_Select_language_button()
+        sleep(10) #time is needed for the cookie button to activate
+        navigationBarObj.click_to_several_times_cookie_button()
+        sleep(10)
+
+
