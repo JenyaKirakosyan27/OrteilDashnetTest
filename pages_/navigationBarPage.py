6efe49367d373c0b3331383cfa8a2b56f0bd2420
Jenya Kirakosyan <jenyakirakosyan27@gmail.com>
2023-12-14 13:09:51 +0400

    Test language select button and click on cookies

--- pages_/navigationBarPage.py
+++ pages_/navigationBarPage.py
@@ -4,14 +4,20 @@ from pages_.basePage import BasePage
 
 class NavigationBarPage(BasePage):
     def __init__(self, driver: webdriver.Chrome):
-       super().__init__(driver)
-       self.__selectLanguageButtonLocator = ()
-       self.__cookieNumberLocator = (By.ID, "cookieNumbers")
+        super().__init__(driver)
+        self.__selectLanguageButtonRuLocator = (By.ID, "langSelect-RU")
+        self.__bigCookieNumberLocator = (By.ID, "bigCookie")
 
     def click_the_Select_language_button(self):
-        selectLanguageButton= self._find_element(self.__selectLanguageButtonLocator)
-        self._click_to_element(selectLanguageButton)
+        selectLanguageButtonRuElement = self._find_element(self.__selectLanguageButtonRuLocator)
+        self._click_to_element(selectLanguageButtonRuElement)
 
-    def click_to_cookie_number(self):
-        cookieNumberElement = self._find_element(self.__cookieNumberLocator)
-        self._click_to_element(cookieNumberElement)
+    def click_to_big_cookie_button(self):
+        bigCookieButtonElement = self._find_element(self.__bigCookieNumberLocator)
+        self._click_to_element(bigCookieButtonElement)
+
+
+    # def click_to_times_cookie_button(self):
+    #     bigCookieButtonElement = self._find_element(self.__bigCookieNumberLocator)
+    #     self._click_to_element(bigCookieButtonElement)
+    #
